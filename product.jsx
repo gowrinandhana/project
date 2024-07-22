bedb1b931cac0e7fc2b4e204cc0be16b34963ce6
gowrinandhana <gowri04nandhana@gmail.com>
2024-07-22 20:35:07 +0530

    Add files via upload

---
+++ product.jsx
@@ -0,0 +1,83 @@
+function Card() {
+    return(<div style={{display:'flex',alignItems:'center',}}>
+      
+        <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./almond.jpg" alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>Almond</p>
+                        <p>price:1000/kg</p>
+            </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./orange.jpg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>Orange</p>
+                        <p>price:100/kg</p>
+            </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./wal.jpg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>walnut</p>
+                        <p>price:2000/kg</p>
+            </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./apple.jpg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>Apple</p>
+                        <p>price:200/ kg</p>
+
+            </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./berry.webp"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>Berry</p>
+                        <p>price:500/kg</p>
+            </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./avacado.jpg"alt=""width='100'height='100px'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>Avacado</p>
+                        <p>price:680/kg</p>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./cherry.jpg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>cherry</p>
+                        <p>price:680/kg</p>
+                        </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./banana.jpg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>banana</p>
+                        <p>price:40/kg</p>
+                        </div>
+            </div>
+            <div style={{display:'flex',gap:'10px',margin:'10px'}}>
+                    <div style={{paddind:'10px',border:'1px solid black'}}>
+                        <img src="./straw.jpeg"alt=""width='100'height='100'></img>
+                        <div style={{fondsize:'30px'}}Title></div>
+                        <p>strawberry</p>
+                        <p>price:500/kg</p>
+                        </div>
+            </div>
+        
+            </div>
+            </div>
+    );
+
+
+}
+export default Card;
